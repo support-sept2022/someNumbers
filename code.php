47d028ef2bbe57195bf9e30b78471f48882a0e0f
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-14 10:48:41 +0100

    Dojo correction v1

--- code.php
+++ code.php
@@ -6,7 +6,16 @@
  */
 function getNegative(int $number): int
 {
+    /*
+    if($number < 0) {
+        return $number;
+    } else {
+        return $number * -1;
+    }
+    */
+    if($number < 0) return $number;
 
+    return $number * -1;
 }
 
 /* This function takes a positive or negative input number. and must increment it by 1 if it is positive and decrement it by 1 if it is negative
@@ -15,17 +24,29 @@ function getNegative(int $number): int
  */
 function incrementOrDecrement(int $number): int
 {
+    if($number < 0) return --$number;
 
+    return ++$number;
 }
 
 /* This function takes a positive or negative input number. and have to count down to 0 if this is negative:
 -4, 'Count: -3 -2 -1 0 Boom!'
 
 If the number is positive then the function returns:
-Sorry but your number is already >= 0
+S>orry but your number is already = 0
 */
 function negativeToZero(int $number): string
 {
+    if($number >= 0) return "Sorry but your number is already >= 0";
 
+    $result = "Count:";
+
+    for ($i = $number + 1; $i <= 0; $i++) {
+        $result .= " $i";
+    }
+
+    $result .= " Boom!";
+
+    return $result;
 }
 
